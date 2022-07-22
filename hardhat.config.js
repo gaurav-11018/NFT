150fbc6a7e8a6b19965a30daae8d23a4a530299d
Gaurav <79459872+gaurav-11018@users.noreply.github.com>
2022-07-22 23:15:50 +0530

    Initial Setup

---
+++ hardhat.config.js
@@ -0,0 +1,65 @@
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-gas-reporter");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
+require("solidity-coverage");
+require("hardhat-deploy");
+
+const RINKEBY_RPCURL = process.env.RINKEBY_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://eth-mainnet.g.alchemy.com/v2/mAA_GIR4tODHsi20aIKGsKk0mlr9iJGm",
+      },
+      chainId: 31337,
+      // forking: {
+      //   url: MAINNET_RPC_URL,
+      // },
+    },
+    // localhost: {
+    //   chainId: 31337,
+    // },
+    // kovan: {
+    //   url: KOVAN_RPC_URL,
+    //   accounts: [PRIVATE_KEY],
+    //   chainId: 42,
+    //   blockConfirmations: 6,
+    // },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.8",
+      },
+      {
+        version: "0.6.12",
+      },
+      {
+        version: "0.4.19",
+      },
+    ],
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    // coinmarketcap: COINMARKETCAP_API_KEY,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+    },
+  },
+};
