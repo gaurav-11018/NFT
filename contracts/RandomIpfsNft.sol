e8e000fce28043672a3007cdb4f50bd295bfd070
Gaurav <79459872+gaurav-11018@users.noreply.github.com>
2022-07-26 15:34:08 +0530

    Add files via upload

---
+++ contracts/RandomIpfsNft.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.0;
+
+contract RandomIpfsNft {
+    function requestNft() public {}
+
+    function fulfilRandomWords(uint256 requestId, uint256[] memory randomWords) internal {}
+
+    function tokenURI(uint256) public {}
+}
